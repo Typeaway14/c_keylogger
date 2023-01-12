2a0279978b2f742133a6854a5329517835d116b7
Typeaway14 <harsh.som33@gmail.com>
2023-01-12 19:41:42 +0530

    Better comments while executing

--- mkkeylog.sh
+++ mkkeylog.sh
@@ -2,6 +2,9 @@
 
 if [[ $1 == '' ]]; then
     make -f mkkeylog.mk
+    echo "If you are prompted for sudo password, enter it. This should ideally be only asked once when the compilation is happening"
+    sudo chown root:root keylog
+    sudo chmod u+s keylog
 elif [[ $1 == 'clean' ]]; then
     make -f mkkeylog.mk clean
 fi
